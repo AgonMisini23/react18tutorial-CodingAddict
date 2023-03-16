36a0cf338e0c4e2c6992096996104d64b57cad23
AgonMisini23 <agon23misini@gmail.com>
2023-03-17 00:56:05 +0100

    Booklist

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,20 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-const Person = () => <h2>john doe</h2>;
-const Message = () => {
-  return <p>this is my message</p>;
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
 };
+
+const Image = () => (
+  <img
+    src='https://images-na.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/713F+ivM9NL._AC_UL300_SR300,200_.jpg'
+    alt='Saved'
+  />
+);
+const Title = () => {
+  return <h2>Saved</h2>;
+};
+
+const Author = () => <h4> Benjamin Hall</h4>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
