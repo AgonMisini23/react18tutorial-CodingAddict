5785e53414d853c6bdcee327527a5c5c5c78e465
AgonMisini23 <agon23misini@gmail.com>
2023-03-17 00:40:29 +0100

    Create Element

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,8 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function Greeting() {
-  return <h2>My First Component</h2>;
+  return (
+    <div>
+      <h2>My First Component</h2>
+    </div>
+  );
 }
+
+// function Greeting() {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h2", {}, "hello world")
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
