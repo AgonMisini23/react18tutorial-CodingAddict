12f2d448abe9621a8f14047f7dfcb62c57a0f257
AgonMisini23 <agon23misini@gmail.com>
2023-03-17 00:42:09 +0100

    JSX Rules

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,20 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function Greeting() {
-  return (
-    <div>
-      <h2>My First Component</h2>
-    </div>
-  );
+  return <h2>hello world</h2>;
 }
-
-// function Greeting() {
-//   return React.createElement(
-//     "div",
-//     {},
-//     React.createElement("h2", {}, "hello world")
-//   );
-// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
